9e2fc0e14dfc7ae5ff8b9a5ae2588fac7751f2d1
DmitriyBodlev <Dmitriybodlev@gmail.com>
2016-12-25 15:17:46 +0200

    add all content and styles

--- src/app/components/pages/federation/federation.component.ts
+++ src/app/components/pages/federation/federation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-federation',
   templateUrl: './federation.component.html',
-  styleUrls: ['./federation.component.css']
+  styleUrls: ['./federation.component.scss']
 })
 export class FederationComponent implements OnInit {
 
